2161660c535a9f2b46ea904aa0ebdcfec70a2f07
donatih55 <donatihenrique@hotmail.com>
2021-12-28 17:22:15 -0600

    Added control buttons to proprieties

--- imageCombinator.js
+++ imageCombinator.js
@@ -16,42 +16,91 @@
 var props = {};
 var combo = {};
 var proprietiesTypes = [];
+var curProprieties = [];
+
+/*********************************************************
+            General functions
+**********************************************************/
+const removeNumber = (arr, num) => arr.filter(el => el !== num);
+
+function addProprietyType(name){
+  if (!proprietiesTypes.includes(name)){
+    proprietiesTypes.push(name);
+  }
+}
+function moveTab(index, isUp){ //isUp = 0 = down,  1 = up
+  var temp = proprietiesTypes[index];
+  proprietiesTypes[index] = proprietiesTypes[index + (isUp > 0 ? -1 : 1)];
+  proprietiesTypes[index + (isUp > 0 ? -1 : 1)] = temp;
+  reloadTabs();
+}
+function deleteTab(index){
+  //dispose of images
+  for(let i of (Object.getOwnPropertyNames(props[proprietiesTypes[index]]))){
+    props[proprietiesTypes[index]].src = '';
+    props[proprietiesTypes[index]] = null;
+  }
+  //remove object
+  delete props[proprietiesTypes[index]];
+  //remove from array with the order
+  proprietiesTypes = removeNumber(proprietiesTypes, proprietiesTypes[index]);
+  //Reload view
+  reloadTabs();
+}
+function addToTab(index){
+  reloadTabs();
+}
+
+/**********************************************************
+                Reload Tabs
+***********************************************************/
+
+function reloadTabs(){
+  document.getElementById('tabs').innerHTML = "";
+  var tabs = document.createElement('div');
+  tabs.classList.add( "tabs" );
+  var t = 0;
+  for (let obj of /*Object.getOwnPropertyNames(props)*/proprietiesTypes){
+    var newTab = document.createElement('div');
+    newTab.classList.add("tab");
+    newTab.innerHTML = '<div class="tabHeader">' + obj + '</div>';
+    newTab.innerHTML +=
+      '<div class="tabControls">'
+      //Up button
+      + (t > 0 ? '<input type="button" value="/\\" class="tabButtonUp" onclick="moveTab('+ t + ', 1)">' : '')
+      //DOwn Button
+      + (t < proprietiesTypes.length - 1 ? '<input type="button" value="\\/" class="tabButtonDown" onclick="moveTab('+ t + ', 0)">' : '')
+      //delete Button
+      + '<input type="button" value="Delete" class="tabButtonDown" onclick="deleteTab('+ t + ')">'
+      //Div for Switiching selected image
+      + '<div class="tabContentSlider" >'
+      ;
+
+      t++;
+    for(let i of (Object.getOwnPropertyNames(props[obj]))){
+      newTab.innerHTML += '<div class="imageContainer"><img src="' + props[obj][i].src + '" width="250px" height="250px"></div>';
+    }
+    newTab.innerHTML += "</div></div>";
+    tabs.appendChild(newTab);
+  }
+  document.getElementById('tabs').innerHTML = tabs.innerHTML;
+}
+
+
+/* After document loaded */
 document.addEventListener("DOMContentLoaded", function(event) {
 
   var canvas = document.getElementById('canvas');
-  canvas.width = 500;
-  canvas.height = 500;
+  canvas.width = 300;
+  canvas.height = 300;
 
-  /**********************************************************
-                  Reload Tabs
-  ***********************************************************/
 
-  function reloadTabs(){
-    document.getElementById('tabs').innerHTML = "";
-    var tabs = document.createElement('div');
-    tabs.classList.add( "tabs" );
-
-    for (let obj of Object.getOwnPropertyNames(props)){
-      var newTab = document.createElement('div');
-      newTab.classList.add("tab");
-      newTab.innerHTML = '<div class="tabHeader">' + obj + '</div>';
-      newTab.innerHTML +=
-        '<div class="tabContentSlider">'
-        + '<input type="button" value="<-" class="tabButtonLeft">'
-        + '<input type="button" value="->" class="tabButtonRight">';
-
-      for(let i of (Object.getOwnPropertyNames(props[obj]))){
-        newTab.innerHTML += '<img src="' + props[obj][i].src + '" width="150px" height="150px">';
-      }
-      newTab.innerHTML += "</div>";
-      tabs.appendChild(newTab);
-    }
-    document.getElementById('tabs').innerHTML = tabs.innerHTML;
-  }
 
   /**********************************************************
                   Parse new Images into props
   ***********************************************************/
+  var countImages = 0;
+  var countImagesLoaded = 0;
   function loadImages() {
     var picker = document.getElementById('inp'); //multi file picker from form
     for(let item of picker.files){
@@ -61,16 +110,23 @@ document.addEventListener("DOMContentLoaded", function(event) {
       img.src = URL.createObjectURL(item);
       img.name = item.name.split('.')[0].replace(/ /g, ''); //just the file name, no extension
       img.type = document.getElementById("propName").value; //The category name on the form/popup
+      countImages++;
     }
   }
   function loadProp() {
     if (!(props.hasOwnProperty(this.type))){
       props[this.type] = {};
+      addProprietyType(this.type);
     }
     props[this.type][this.name] = this;
+    countImagesLoaded++;
+    if (countImages = countImagesLoaded){
+      reloadTabs();
+    }
   }
   function failed() {
     console.error("The provided file couldn't be loaded as an Image media");
+    countImagesLoaded++;
   }
 /**********************************************************
                       On Click Events
@@ -80,15 +136,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
     loadImages();
   };
   document.getElementById('test').onclick = function(e){
-    for (let obj of Object.getOwnPropertyNames(props)){
+    for (let obj of /*Object.getOwnPropertyNames(props)*/ proprietiesTypes){
       for(let i of (Object.getOwnPropertyNames(props[obj]))){
-        document.getElementById('canvas').getContext('2d').drawImage(props[obj][i], 0, 0, 500, 500);
+        document.getElementById('canvas').getContext('2d').drawImage(props[obj][i], 0, 0, 300, 300);
       }
     }
   };
   document.getElementById('newTab').onclick = function(e){
     document.getElementById('newTabPopup').classList.remove("invisible");
-
+    document.getElementById('propName').value = "";
+    //if (document.getElementById('inp').files.length > 0){
+      document.getElementById('inp').value = null;
+    //}
   };
   document.getElementById('download').onclick = function(e){
     /*const url = $("#canvas").get(0).toDataURL("img/png");
@@ -106,7 +165,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
       var template = {};
       template.canvas = document.createElement('canvas');
-      proprietiesTypes = Object.getOwnPropertyNames(props);
+      //proprietiesTypes = Object.getOwnPropertyNames(props);
       var oneImage = props[proprietiesTypes[0]][Object.getOwnPropertyNames(props[proprietiesTypes[0]])[0]];
       template.canvas.width = oneImage.width;
       template.canvas.height = oneImage.height;
@@ -123,12 +182,27 @@ document.addEventListener("DOMContentLoaded", function(event) {
       // the filename you want
       a.download = 'Test_image.png';
       document.body.appendChild(a);
+
+      var csvFile = "";
       for(let i of Object.getOwnPropertyNames(combo)){
         a.href = $(combo[i].canvas).get(0).toDataURL("img/png");
+        csvFile += i + ',';
+        for(let j of Object.getOwnPropertyNames(combo[i])){
+          if (j != "canvas"){
+            csvFile += j + ',' + combo[i][j] + ',';
+          }
+        }
+        csvFile += '\n';
         console.log("Saving2: " + i);
         a.click();
       }
+      a.href = 'data:attachment/text,' + encodeURI(csvFile);
+      a.download = 'test.csv';
+      a.click();
     };
+    /**********************************************************
+                  All possible combinations generator
+    ***********************************************************/
 
     function recurseProps(proprieties, key, ret, headerIndex){
         if(headerIndex == proprietiesTypes.length) {
